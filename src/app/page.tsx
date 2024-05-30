cb683c6a6185df952008f40b7ae658c414a481fa
Suvash Joshi <suvash2077@gmail.com>
2024-05-30 15:01:15 +0545

    basic UI with mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
-import Link from "next/link";
+const mockUrls = [
+  "https://utfs.io/f/f84d159b-4cbe-41bc-9435-521dc348bbe3-9yg51o.jpg",
+  "https://utfs.io/f/23365aff-1ee9-4cce-b900-867e4480dd62-9yg51p.jpg",
+  "https://utfs.io/f/6ada3bc3-6cfa-422f-830a-a01ad658b3aa-gv1znd.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
       hello modern react
     </main>
   );
